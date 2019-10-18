f7d6eadb46ea6113651b7c4616dcaa74985f48fb
nalcala <alcalan@fellows.iarc.fr>
2019-10-18 16:34:35 +0200

    added ballgown object rdata to output

--- bin/create_matrices.R
+++ bin/create_matrices.R
@@ -1,13 +1,15 @@
 require(ballgown)
 samples = list.dirs(".")
 samples = gsub("./", "", samples[samples!="."])
-my.data = ballgown(samples=samples, dataDir=".", meas='all')
-gdata = gexpr(my.data)
+bg = ballgown(samples=samples, dataDir=".", meas='all')
+gdata = gexpr(bg)
 gdata = cbind(rownames(gdata),gdata)
 colnames(gdata)[1] = "gene_id"
-tdata = texpr(my.data)
-tdata = cbind(transcriptNames(my.data),tdata)
+tdata = texpr(bg)
+tdata = cbind(transcriptNames(bg),tdata)
 colnames(tdata)[1] = "transcript_id"
+
+#write tables and R object
 write.csv(gdata,"gene_FPKM_matrix.csv",quote=F,row.names = F)
 write.csv(tdata,"transcript_FPKM_matrix.csv",quote=F,row.names = F)
-	
+save(bg,file="bg.rda")
