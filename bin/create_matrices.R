9c20712fee3c26d4bcd2a951948783d538a96077
nalcala <alcalan@fellows.iarc.fr>
2019-10-18 12:59:18 +0200

    debug FPKM matrix creation

--- bin/create_matrices.R
+++ bin/create_matrices.R
@@ -1,8 +1,13 @@
 require(ballgown)
-my.data = ballgown(dataDir=".", meas='all')
+samples = list.dirs(".")
+samples = gsub("./", "", samples[samples!="."])
+my.data = ballgown(samples=samples, dataDir=".", meas='all')
 gdata = gexpr(my.data)
+gdata = cbind(rownames(gdata),gdata)
+colnames(gdata)[1] = "gene_id"
 tdata = texpr(my.data)
-rownames(tdata) = transcriptNames(my.data)
-write.csv(gdata,"gene_FPKM_matrix.csv",quote=F)
-write.csv(tdata,"transcript_FPKM_matrix.csv",quote=F)
+tdata = cbind(transcriptNames(my.data),tdata)
+colnames(tdata)[1] = "transcript_id"
+write.csv(gdata,"gene_FPKM_matrix.csv",quote=F,row.names = F)
+write.csv(tdata,"transcript_FPKM_matrix.csv",quote=F,row.names = F)
 	
